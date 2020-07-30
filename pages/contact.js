3fb7813edc23348f35f24df3bb51b4c78f7c162b
sali mohamed <sally.elgendy@hotmail.com>
2020-07-30 15:41:38 -0400

    recognize css file

--- pages/contact.js
+++ pages/contact.js
@@ -8,7 +8,7 @@ const Contact = () => {
                     <h1>CONTACT ME</h1>
                     <div className='myoffer'>
                         <p>I am available for hire and open to any ideas of cooperation.</p>
-                        <ul>
+                        <ul className='contact-h5'>
                             <li><h5>E-mail: <a href='https://www.hotmail.com'> sally.elgendy@hotmail.com</a></h5></li>
                             <li><h5>LinkedIn: <a href='https://www.linkedin.com/in/sali-m-aa7a95b4/' >Sali Mohamed</a></h5></li>
                             <li><h5>Github: <a href='https://github.com/sali73'>sali73</a></h5></li>
