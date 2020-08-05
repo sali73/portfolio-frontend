d29304e6d86a7f600714200fa7d25b97bc2f972e
sali mohamed <sally.elgendy@hotmail.com>
2020-08-04 22:48:47 -0400

    change some data

--- pages/my_offer.js
+++ pages/my_offer.js
@@ -22,11 +22,15 @@ const Offer = () => {
                         <h5><img src='https://icons.iconarchive.com/icons/cornmanthe3rd/plex/24/Other-html-5-icon.png'/> FRONT-END ENGINEERING</h5>
                         <h6>My current experience and skills in front-end includes:</h6>
                         <ul>
-                            <li><h6> React</h6></li>
-                            <li><h6> Next.js</h6></li>
-                            <li><h6> Redux</h6></li>
+                        HTML, CSS, SASS, JavaScript, jQuery, Bootstrap, React JS, Next.js, Node.js, Angular JS, RESTful Web API’s , MySQL, MongoDb and Postgres , JSON, AJAX, Python, Django, Express
+                            <li><h6> HTML, CSS, SASS</h6></li>
+                            <li><h6> javascript</h6></li>
                             <li><h6> jQuery</h6></li>
-                            <li><h6> HTML, Css, sess, javascript</h6></li>
+                            <li><h6> React.JS</h6></li>
+                            <li><h6> Next.js</h6></li>
+                            <li><h6> Angular</h6></li>
+                            <li><h6> Node.js </h6></li>
+                            <li><h6> Express</h6></li>
                         </ul>
                     </div>
                     <div className='line'></div>
@@ -39,11 +43,10 @@ const Offer = () => {
                             <li><h6> using different types of databases (like PostgreSQL, MySQL)</h6></li>
                             <li><h6> python</h6></li>
                             <li><h6>Django</h6></li>
-                            <li><h6>Express</h6></li>
                         </ul>
                     </div>
                     <div className='line'></div>
-                    <h6 style={{color:'#4A5665'}}>If your initial thought is that I can’t be the best in everything, it's okay. Send the design job to someone else, and I will take care of all the code development, or whatever other role you see me best in.
+                    <h6 style={{color:'#7c8591', fontFamily:'Times New Roman'}}>If your initial thought is that I can’t be the best in everything, it's okay. Send the design job to someone else, and I will take care of all the code development, or whatever other role you see me best in.
                         In cases when you already have an existing team, but you would still use some help, <Link href={'/contact'}> Contact me. </Link> as well. I’ll fit in very quickly, I promise.</h6>
                 </div>
             </Layout>
