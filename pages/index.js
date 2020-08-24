f73e2895fc4245cb0fe6dba5a0d7593c255e7bb4
sali73 <sally.elgendy@hotmail.com>
2020-08-23 22:06:42 -0400

    change card size

--- pages/index.js
+++ pages/index.js
@@ -62,11 +62,11 @@ render(){
                                 ? this.props.projects.map((index, key) => {
                                     return (
                                             <>
-                                                <div className='card' style={{width: "25rem" }} key={key} >
+                                                <div className='card' style={{width: "22rem" }} key={key} >
                                                         <img className='card-img-top projectsimg'src={index.image} alt="photo"/>
                                                     <div className="card-body">
                                                         <h4 className="card-title">{index.title}</h4>
-                                                        <div style={{display: "flex" }}>
+                                                        <div style={{display: "flex", flexWrap:"wrap" }}>
                                                         <button type="button" className="btn btn-light"><a href={index.frontend} ><h6> Frontend Code</h6> </a></button>
                                                         <button type="button" className="btn btn-light"><a href={index.backend} ><h6> Backend Code</h6> </a></button>
                                                         <button type="button" className="btn btn-lightbtn btn-light"><a href= {index.deployment}><h6>Deployment </h6></a></button>
