922b0062da15f13841005be0b5d3d7471df182a6
sali73 <sally.elgendy@hotmail.com>
2020-11-02 17:39:54 -0500

    create conect form

--- pages/contact.js
+++ pages/contact.js
@@ -8,6 +8,27 @@ const Contact = () => {
                     <div className="port">
                         <h1>CONTACT ME</h1>
                     </div>
+                    <form action="action_page.php">
+
+                        <label for="fname">First Name</label>
+                        <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+                        <label for="lname">Last Name</label>
+                        <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+                        <label for="country">Country</label>
+                        <select id="country" name="country">
+                        <option value="australia">USA</option>
+                        <option value="canada">Egypt</option>
+                        </select>
+
+                        <label for="subject">Subject</label>
+                        <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+
+                        <input type="submit" value="Submit">
+
+                    </form>
+                    
                     <div className='my-offer'>
                         <p>I am available for hire and open to any ideas of cooperation.</p>
                         <ul className='contact-h5'>
