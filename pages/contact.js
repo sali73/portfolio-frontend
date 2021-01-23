df13970d6bf96e3e38f9c1482c6f513138ead805
sali73 <sally.elgendy@hotmail.com>
2021-01-23 13:24:39 -0500

    more style for contact  form

--- pages/contact.js
+++ pages/contact.js
@@ -12,40 +12,52 @@ const Contact = () => {
                         <h1> I am available for hire and open to any ideas of cooperation</h1>
                         <div className='form-row'>
                             <div class="form-group col-md-6">
-                                <input type="text" name="name"  placeholder="Name"/><br/>
+                                <input type="text" name="name"  placeholder="Enter Your Name"/><br/>
                             </div>
                             <div class="form-group col-md-6">
-                                <input type="text" name="mail" placeholder='E-mail'/><br/>
+                                <input type="text" name="mail" placeholder='Enter Your Email'/>
+                                <br/>
                             </div>
                         </div>
-                        <fieldset class="form-group">
+                        <fieldset class="form-group " >
                             <div class="row">
-                                <legend style={{paddingLeft:'35px'}} className="col-form-label col-sm-2 pt-0">Gender:</legend>
+                            <section class="light">
+                                <legend style={{ marginLeft:'80px' , color: '#B31D72', fontSize: '22px'}}>Gender:</legend>
                                 <div class="col-sm-10">
                                     <div class="form-check">
-                                        <input class="form-check-input" type='radio' name="gender" value="female" id="gridRadios1"  checked />
-                                        <label class="form-check-label" for="gridRadios1">
-                                        Female
-                                        </label>
-                                    </div>
-                                    <div class="form-check">
+                                    <label for="gridRadios1">
+                                        <input  type='radio' name="gender" value="female" id="gridRadios1"  checked />
+                                        <span class="design"></span>
+                                         <span class="text">Female</span>
+                                    </label>
+                                </div>
+                                <div class="form-check">
+                                    <label class="form-check-label" for="gridRadios2" >
                                         <input class="form-check-input" type='radio' name="gender" value="male" id="gridRadios2" />
-                                        <label class="form-check-label" for="gridRadios2">
-                                        Male
+                                        <span class="design"></span>
+                                        <span class="text"> Male</span>
+                                       
                                         </label>
                                     </div>
                                 </div>
+                                </section>
                             </div>
                         </fieldset>
                      
                         <div class="form-group">
-                            <textarea style={{ width:'650px', padding:'20px' , margin:'20px'}} name="comment" placeholder="Comment....." className="form-control" rows='4' ></textarea><br/>
+                            <textarea style={{ width:'650px', padding:'20px' , margin:'50px 20px 0px'}} name="comment" placeholder="Comment....." className="form-control" rows='5' ></textarea><br/>
                         </div>
                         <hr/>
                          <div style={{ textAlign:'center'}}>   
                             <button type="submit" > Submit</button>
                             <button type="reset"> Reset</button>
-                        </div>                     
+                        </div>  
+
+
+
+
+                        
+                                           
                     </form>
                     </div>
                     <div className='social-media'>
